02591e2ade6e52a10adb7d0d601d83f36a393943
Oj <oj@oojmed.com>
2021-07-15 09:25:38 +0100

    [Storage] Initial Add

--- background.js
+++ background.js
@@ -30,3 +30,41 @@ chrome.webRequest.onHeadersReceived.addListener(({ responseHeaders, url }) => {
 
   ['blocking', 'responseHeaders']
 );
+
+
+// Extension Storage (v10)
+const storageCache = {}; // Based off storage cache object
+
+chrome.storage.local.get(null, (data) => {
+  storageCache = data;
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.storage) { // Storage obj
+    const { type, key, value } = request.storage;
+
+    switch (type) {
+      case 'set': {
+        const obj = {}; // Make object for set
+        obj[key] = value;
+
+        chrome.storage.local.set(obj); // Set real storage
+        storageCache[key] = value; // Set cache (mostly unneeded but)
+
+        break;
+      }
+
+      case 'get': {
+        sendResponse(storageCache);
+
+        break;
+      }
+
+      case 'remove': {
+        chrome.storage.local.remove(key);
+
+        break;
+      }
+    }
+  }
+});
--- content.js
+++ content.js
@@ -1,7 +1,7 @@
-const inject = async () => {
+const inject = async (branch) => {
   console.log('[GooseMod for Web] Injecting...');
 
-  // Re-define localStorage as Discord removes it
+  /* // Re-define localStorage as Discord removes it
   function getLocalStoragePropertyDescriptor() {
     const frame = document.createElement('frame');
     frame.src = 'about:blank';
@@ -17,14 +17,15 @@ const inject = async () => {
   
   Object.defineProperty(window, 'localStorage', getLocalStoragePropertyDescriptor());
   
-  console.log('[GooseMod for Web] Redefined localStorage');
+  console.log('[GooseMod for Web] Redefined localStorage'); */
 
   const branchURLs = {
-    'release': 'https://api.goosemod.com/inject.js',
-    'dev': 'https://updates.goosemod.com/guapi/goosemod/dev'
+    release: 'https://api.goosemod.com/inject.js',
+    dev: 'https://updates.goosemod.com/guapi/goosemod/dev',
+    local: 'http://localhost:1234/index.js'
   };
   
-  const branch = localStorage.getItem('goosemodUntetheredBranch') || 'release';
+  // const branch = storageCache['goosemodUntetheredBranch'] || 'local'; // 'release';
 
   console.log('[GooseMod for Web] Branch =', branch);
   console.log('[GooseMod for Web] JS Url =', branchURLs[branch]);
@@ -40,10 +41,33 @@ const inject = async () => {
   console.log('[GooseMod for Web] Injected fetched JS');
 };
 
-// Delay actual injection to fix FF issues
 
-let el = document.createElement('script');
+// Extension Storage (v10)
+let storageCache = {};
 
-el.appendChild(document.createTextNode(`(${inject.toString()})()`));
+chrome.runtime.sendMessage({ storage: { type: 'get' } }, (response) => {
+  storageCache = response; // Set storageCache
 
-document.body.appendChild(el);
+  // Delay actual injection to fix FF issues
+
+  const el = document.createElement('script');
+
+  el.appendChild(document.createTextNode(`(${inject.toString()})(${JSON.stringify(storageCache['goosemodUntetheredBranch'] || 'release')})`));
+
+  document.body.appendChild(el);
+});
+
+
+document.addEventListener('gmes_get', ({ }) => {
+  document.dispatchEvent(new CustomEvent('gmes_get_return', storageCache));
+});
+
+document.addEventListener('gmes_set', ({ key, value }) => {
+  storageCache[key] = value; // Repopulate cache with updated value
+  chrome.runtime.sendMessage({ storage: { type: 'set', key, value } }); // Actually store change
+});
+
+document.addEventListener('gmes_remove', ({ key }) => {
+  delete storageCache[key]; // Repopulate cache with updated value
+  chrome.runtime.sendMessage({ storage: { type: 'remove', key } }); // Actually store change
+});
