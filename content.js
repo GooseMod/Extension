f12a5295986ab19b3aa9aa6a6bd1598200df124a
Oj <oj@oojmed.com>
2021-08-10 17:24:06 +0100

    [Chore] Revert the revert (yes)

--- content.js
+++ content.js
@@ -7,7 +7,7 @@ const inject = async (branch, version) => {
 
   const branchURLs = {
     release: 'https://api.goosemod.com/inject.js',
-    dev: 'https://updates.goosemod.com/guapi/goosemod/dev',
+    dev: 'https://raw.githubusercontent.com/GooseMod/GooseMod/dist-dev/index.js',
     local: 'http://localhost:1234/index.js'
   };
 
